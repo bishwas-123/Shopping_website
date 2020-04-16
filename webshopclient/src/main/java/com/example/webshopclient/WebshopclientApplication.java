82dbd2c193cb59a3ff1d5110bee906502ce53615
Bishwas Niraula <bishwash.niraula@gmail.com>
2020-04-15 22:57:24 -0400

    project created
    project created

---
+++ webshopclient/src/main/java/com/example/webshopclient/WebshopclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.webshopclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopclientApplication.class, args);
+    }
+
+}
---
+++ webshopclient/src/test/java/com/example/webshopclient/WebshopclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webshopclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
